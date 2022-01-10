b1a12a3e4b652f86b537bfabdf7f6c4b25e093ed
May Elazar <maye@my.hit.ac.il>
2022-01-10 13:06:19 +0200

    adjust form

--- src/components/contectme.js
+++ src/components/contectme.js
@@ -57,6 +57,38 @@ function Contectme() {
           </button>
         </form>
       </div>
+
+      {/********************************* */}
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+      {/************************************** */}
       <div className="bg-white-whiteGray">
         <div className="hidden md:flex  p-4 flex-row justify-evenly">
           <div className="flex ">
