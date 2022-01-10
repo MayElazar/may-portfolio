78b25c71e13a273a9bf56b72714cb09c140b3d4b
May Elazar <maye@my.hit.ac.il>
2022-01-10 13:12:05 +0200

    adjust form

--- src/components/contectme.js
+++ src/components/contectme.js
@@ -14,6 +14,7 @@ function Contectme() {
       </p>
       <div className="flex justify-center">
         <form
+          action="POST"
           className="flex flex-col p-5"
           name="contact"
           method="POST"
@@ -58,37 +59,6 @@ function Contectme() {
         </form>
       </div>
 
-      {/********************************* */}
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-      {/************************************** */}
       <div className="bg-white-whiteGray">
         <div className="hidden md:flex  p-4 flex-row justify-evenly">
           <div className="flex ">
